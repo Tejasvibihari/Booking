f53efd872c9f46b08f2e3f6c0978e3d875f005e6
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-03-02 10:45:21 +0530

    Readme.md file Added

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Booking Web App
+
+A brief description of what this project does and who it's for
+
+
+
+## Authors
+
+- [@Tejasvi Bihari](https://www.github.com/Tejasvibihari)
+
+## Color Reference
+
+| Color             | Hex                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| Dark spring green | ![#1C7C54](https://via.placeholder.com/10/1C7C54?text=+) #1C7C54 |
+| Celadon | ![#73E2A7](https://via.placeholder.com/10/173E2A7?text=+) #173E2A7 |
+| TeaGreen | ![#DEF4C6](https://via.placeholder.com/10/DEF4C6?text=+) #DEF4C6 |
+| Cal Poly Green | ![#1B512D](https://via.placeholder.com/10/DEF4C6?text=+) #1B512D |
+| Yellow Green | ![#B1CF5F](https://via.placeholder.com/10/1B512D?text=+) #1B512D |
+
--- client/src/components/ui/TypewriterEffectSmoothDemo.jsx
+++ client/src/components/ui/TypewriterEffectSmoothDemo.jsx
@@ -1,40 +1,42 @@
 "use client";
 import { TypewriterEffectSmooth } from "./typewriter-effect";
 export default function TypewriterEffectSmoothDemo() {
-    const words = [
-        {
-            text: "Build",
-            className: "text-white"
-        },
-        {
-            text: "awesome",
-            className: "text-white"
-        },
-        {
-            text: "apps",
-            className: "text-white"
-        },
-        {
-            text: "with",
-            className: "text-white"
-        },
-        {
-            text: "Booking.com",
-            className: "text-[#B1CF5F] dark:text-blue-500",
-        },
-    ];
-    return (
-        <div className="flex flex-col items-center justify-center h-[40rem]  ">
-            <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-                The road to freedom starts from here
-            </p>
-            <TypewriterEffectSmooth words={words} />
-            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-                <button className="w-40 h-10 rounded-xl bg-black dark:border-white border-transparent text-white text-sm">
-                    Join now
-                </button>
-                <button className="w-40 h-10 text-black px-7 py-2 text-center bg-[#73E2A7] text-sm rounded-full hover:bg-black hover:text-white hover:outline-red-800 transition-all duration-300">Sign in</button>
-            </div>
-        </div>
-    );
+  const words = [
+    {
+      text: "Build",
+      className: "text-white",
+    },
+    {
+      text: "awesome",
+      className: "text-white",
+    },
+    {
+      text: "apps",
+      className: "text-white",
+    },
+    {
+      text: "with",
+      className: "text-white",
+    },
+    {
+      text: "Booking.com",
+      className: "text-[#B1CF5F] dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+        The road to freedom starts from here
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <button className="w-40 h-10 rounded-full bg-black border-white border-[1px] text-white text-sm hover:bg-[#73E2A7] hover:text-black hover:border-none  transition-all duration-300">
+          Explore
+        </button>
+        <button className="w-40 h-10 text-black px-7 py-2 text-center bg-[#73E2A7] text-sm rounded-full hover:bg-black hover:text-white hover:border-[1px] border-white transition-all duration-300">
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
 }
