25ae3a5639116a5951e88955562bdc5235c386f9
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-03-13 09:59:27 +0530

    Loop through storeHotel data and map on the page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,13 +1,29 @@
 import AdminDashboard from "./AdminDashboard";
 import AddHotelForm from "../components/Dashboard/AddHotelForm";
 import HotelCard from "../components/Dashboard/HotelCard";
+import { useSelector } from "react-redux";
+
 
 export default function Listing() {
+    const { storeHotel } = useSelector((state) => state.hotel);
+    console.log(storeHotel);
     return (
         <>
             <AdminDashboard>
                 <AddHotelForm />
-                <HotelCard />
+                {storeHotel.map((hotel, index) => {
+                    return (
+                        <HotelCard
+                            key={index}
+                            hotelName={hotel.hotelName}
+                            city={hotel.city}
+                            state={hotel.state}
+                            basePrice={hotel.basePrice}
+                            geolocation={hotel.geolocation}
+                        />)
+                })
+
+                }
             </AdminDashboard></>
     )
 }
